a19fe335e095908aa1d6bb0a8dd0e85e5d468e67
FadhilahAA <112860183+Fadhilah25@users.noreply.github.com>
2022-09-12 20:56:34 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+     /*Fadhilah Aditya Akbar
+    A11.2022.14130/A11.14104
+    */
+    int u = 19;
+    float a = 173.5; //kayaknya
+    char b = 'L';
+    string c = "XL";
+        cout << "Umur saya: " << u;
+        cout << endl;
+        cout << "Umur saya: " << a;
+        cout << endl;
+        cout << "Ukuran baju saya: " << b;
+        cout << endl;
+        cout << "lebar celana: " << c;
+
+    return 0;
+}
